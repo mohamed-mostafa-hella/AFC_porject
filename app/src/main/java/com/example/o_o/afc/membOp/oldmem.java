bab8ba0fa615569c5c8b0b078d0a89c161ad632a
mohamed <hoomos98@gmail.com>
2019-04-29 12:08:48 +0200

    //break

---
+++ app/src/main/java/com/example/o_o/afc/membOp/oldmem.java
@@ -0,0 +1,15 @@
+package com.example.o_o.afc.membOp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.o_o.afc.R;
+
+public class oldmem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oldmem);
+    }
+}
