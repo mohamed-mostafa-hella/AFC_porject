1e9d2bf81496f86125dbf2413a9c68a6a3ae622d
mohamed <hoomos98@gmail.com>
2019-04-29 15:19:53 +0200

    //break

---
+++ app/src/main/java/com/example/o_o/afc/membOp/editmem.java
@@ -0,0 +1,28 @@
+package com.example.o_o.afc.membOp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import com.example.o_o.afc.R;
+
+public class editmem extends AppCompatActivity {
+    Toolbar mtoolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editmem);
+        mtoolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(mtoolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+
+    }
+}
--- app/src/main/java/com/example/o_o/afc/membOp/findmem.java
+++ app/src/main/java/com/example/o_o/afc/membOp/findmem.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.example.o_o.afc.R;
 
-public class oldmem extends AppCompatActivity {
+public class findmem extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_oldmem);
+        setContentView(R.layout.findmem);
     }
 }
