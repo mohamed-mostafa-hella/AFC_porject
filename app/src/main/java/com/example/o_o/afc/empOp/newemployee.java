2ff1d1a89c317a47bd88285d64909be93cc26a5b
mohamed <hoomos98@gmail.com>
2019-04-30 22:48:08 +0200

    //ids

--- app/src/main/java/com/example/o_o/afc/empOp/newemployee.java
+++ app/src/main/java/com/example/o_o/afc/empOp/newemployee.java
@@ -2,14 +2,20 @@ package com.example.o_o.afc.empOp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.o_o.afc.R;
 
 public class newemployee extends AppCompatActivity {
-
+    String [] Jobs={"مدير النادي","نائب مديرالنادي ","مدير شئون قانونية","باحث بشئون قانونية","مدير نشاط رياضي","باحث بالنشاط الرياضي","مدير آمن","فرد آمن","مدير العضويات","باحث العضويات","رئيس شئون العاملين","موظف شئون عاملين","مسئول نظافة","مدير ثقافى ورحلات","باحث ثقافى ورحلات","مدرب"};
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newemployee);
+        spinner=findViewById(R.id.jobs);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,Jobs);
+     spinner.setAdapter(adapter);
     }
 }
