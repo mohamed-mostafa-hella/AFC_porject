b6fb175b61265837ca05b5af5c2503479490f18a
mohamed <hoomos98@gmail.com>
2019-05-01 16:34:17 +0200

    //bit_map O_o

--- app/src/main/java/com/example/o_o/afc/visOp/newVistor.java
+++ app/src/main/java/com/example/o_o/afc/visOp/newVistor.java
@@ -1,15 +1,34 @@
 package com.example.o_o.afc.visOp;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
+import android.widget.ImageView;
 
 import com.example.o_o.afc.R;
 
+import net.glxn.qrgen.javase.QRCode;
+
 public class newVistor extends AppCompatActivity {
+    ImageView myImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_vistor);
+        String myBitmapString = QRCode.from("mohamed25").toString();
+
+        myImage = (ImageView) findViewById(R.id.ticketcode);
+
+
+        byte[] ecode = Base64.decode(myBitmapString , Base64.DEFAULT);
+        Bitmap myBitMap = BitmapFactory.decodeByteArray(ecode , 0  , ecode.length );
+
+
+
+        myImage.setImageBitmap(myBitMap);
+
     }
 }
