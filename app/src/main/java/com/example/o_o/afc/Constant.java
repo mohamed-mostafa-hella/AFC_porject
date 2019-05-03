11023225f573dbd57ceaf14347f2fe57db3bb8f6
mohamed <hoomos98@gmail.com>
2019-05-03 18:53:40 +0200

    //habd

--- app/src/main/java/com/example/o_o/afc/Constant.java
+++ app/src/main/java/com/example/o_o/afc/Constant.java
@@ -1,5 +1,9 @@
 package com.example.o_o.afc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Constant {
     public static final String TYPE="type";
     public static final int EM1=1;
@@ -9,6 +13,8 @@ public class Constant {
 
     public static final String SHARDPREFNAME="mmhshardprfrANSE";
 
-    public static final String[] Jobs = {"مدير النادي", "نائب مديرالنادي ", "مدير شئون قانونية", "باحث بشئون قانونية", "مدير نشاط رياضي", "باحث بالنشاط الرياضي", "مدير آمن", "فرد آمن", "مدير العضويات", "باحث العضويات", "رئيس شئون العاملين", "موظف شئون عاملين", "مسئول نظافة", "مدير ثقافى ورحلات", "باحث ثقافى ورحلات", "مدرب"};
+    public static final List<String> Jobs =new ArrayList<String>(Arrays.asList(new String[]{"مدير النادي", "نائب مديرالنادي "
+            , "مدير شئون قانونية", "باحث بشئون قانونية", "مدير نشاط رياضي", "باحث بالنشاط الرياضي", "مدير آمن", "فرد آمن", "مدير العضويات", "باحث العضويات", "رئيس شئون العاملين", "موظف شئون عاملين", "مسئول نظافة", "مدير ثقافى ورحلات", "باحث ثقافى ورحلات", "مدرب"}));
+
 
 }
